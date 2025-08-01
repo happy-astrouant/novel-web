0f80dc69cb9ec672baa4df81e8a5295e131266b9
谁解幽人幽意 <2634118472@qq.com>
2025-08-01 15:03:23 +0800

    实现基础配置类

--- src/main/java/com/xzy/novel/NovelApplication.java
+++ src/main/java/com/xzy/novel/NovelApplication.java
@@ -1,7 +1,10 @@
 package com.xzy.novel;
 
+import com.xzy.novel.core.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
 public class NovelApplication {
---
+++ src/main/java/com/xzy/novel/core/common/constant/CommonConsts.java
@@ -0,0 +1,54 @@
+package com.xzy.novel.core.common.constant;
+
+/**
+ * 通用常量
+ *
+ */
+public class CommonConsts {
+
+    /**
+     * 是
+     * */
+    public static final Integer YES = 1;
+
+    /**
+     * 否
+     * */
+    public static final Integer NO = 0;
+
+    /**
+     * 性别常量
+     * */
+    public enum SexEnum{
+
+        /**
+         * 男
+         * */
+        MALE(0,"男"),
+
+        /**
+         * 女
+         * */
+        FEMALE(1,"女");
+
+        SexEnum(int code,String desc){
+            this.code = code;
+            this.desc = desc;
+        }
+
+        private int code;
+        private String desc;
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+    }
+
+    // ...省略若干常量
+}
+
