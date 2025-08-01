7e827021d80e1bcad3781db46b3a34281729a050
谁解幽人幽意 <2634118472@qq.com>
2025-08-02 00:20:49 +0800

    加入Caffeine缓存

---
+++ src/main/java/com/xzy/novel/core/config/CacheConfig.java
@@ -0,0 +1,39 @@
+package com.xzy.novel.core.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.xzy.novel.core.common.constant.CacheConsts;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class CacheConfig {
+
+    /**
+     * Caffeine 缓存管理器
+     */
+    @Bean
+    public CacheManager caffeineCacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+
+        List<CaffeineCache> caches = new ArrayList<>(CacheConsts.CacheEnum.values().length);
+        for (CacheConsts.CacheEnum c : CacheConsts.CacheEnum.values()) {
+            if (c.isLocal()) {
+                Caffeine<Object, Object> caffeine = Caffeine.newBuilder().recordStats().maximumSize(c.getMaxSize());
+                if (c.getTtl() > 0) {
+                    caffeine.expireAfterWrite(Duration.ofSeconds(c.getTtl()));
+                }
+                caches.add(new CaffeineCache(c.getName(), caffeine.build()));
+            }
+        }
+
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
+}
